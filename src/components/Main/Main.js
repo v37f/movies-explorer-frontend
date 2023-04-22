6a77e5c28b3ac1977289986b7e29457a8fedf34a
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-22 21:30:49 +0300

    feat: add some main components

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,10 @@
 import Promo from "../Promo/Promo";
 import NavTab from "../NavTab/NavTab";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
+import Portfolio from "../Portfolio/Portfolio";
+import Footer from "../Footer/Footer";
 
 function Main() {
   return (
@@ -8,6 +12,10 @@ function Main() {
       <Promo />
       <NavTab />
       <AboutProject />
+      <Techs />
+      <AboutMe />
+      <Portfolio />
+      <Footer />
     </main>
   );
 }

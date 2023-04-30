b948b11d8d0be055986756aa48dc20fa610c5bf3
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-30 14:44:51 +0300

    feat: add profile component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
 import Footer from "../Footer/Footer";
 import { useLocation, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -26,6 +27,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./Profile.css";
+
+function Profile() {
+  const userName = "Владимир"
+  const email = "randim@yandex.ru"
+
+  const [isFormDisabled, setIsFormDisabled] = useState(true)
+  const isButtonDisabled = false;
+
+  function handleEditClick() {
+    setIsFormDisabled(!isFormDisabled);
+  }
+
+  return (
+    <main className="profile">
+      <div className="profile__container">
+        <h2 className="profile__greetings">Привет, {userName}!</h2>
+        <form className="profile__form">
+          <fieldset className="profile__form-fieldset" disabled={isFormDisabled} >
+            <label className="profile__form-label" for="username">
+              <span className="profile__form-label-span">Имя</span>
+              <input className="profile__form-input" type="text" id="username" name="username" defaultValue={userName} /></label>
+            <label className="profile__form-label" for="email">
+              <span className="profile__form-label-span">E-mail</span>
+              <input className="profile__form-input" type="email" id="email" name="email" defaultValue={email} />
+            </label>
+          </fieldset>
+          {!isFormDisabled && 
+            <span className={"profile__form-error" + (isButtonDisabled ? " profile__form-error_visible" : "")}>
+              При обновлении профиля произошла ошибка.
+            </span>
+          }
+          {!isFormDisabled && <button className="profile__form-button" type="submit" onClick={handleEditClick} disabled={isButtonDisabled} >Сохранить</button>}
+        </form>
+        {isFormDisabled && <button className="profile__button" type="button" onClick={handleEditClick} >Редактировать</button>}
+        {isFormDisabled && <button className="profile__button profile__button_color_red" type="button">Выйти из аккаунта</button>}
+      </div>
+    </main>
+  );
+}
+
+export default Profile;

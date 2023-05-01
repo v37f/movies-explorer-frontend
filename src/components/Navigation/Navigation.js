8cbcbd5147e116238fa63297a3456f1d13f48c4c
Vladimir Frolov <frolov_vo@inbox.ru>
2023-05-01 16:32:55 +0300

    style: remove comments

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -12,20 +12,6 @@ function Navigation() {
           <NavLink className="navigation__link" to="/saved-movies">Сохранённые фильмы</NavLink>
         </li>
       </ul>
-      {/* {isLoggedIn 
-        ? 
-          <ul className="navigation__links">
-            <Link className="navigation__link" to="/movies">Фильмы</Link>
-            <Link className="navigation__link" to="/saved-movies">Сохранённые фильмы</Link>
-            <Link className="navigation__link navigation__link_type_icon" to="/profile">Аккаунт</Link>
-          </ul>
-        : 
-          <ul className="navigation__links navigation__links_type_unlogged">
-            <Link className="navigation__link navigation__link_type_small" to="/signup">Регистрация</Link>
-            <Link className="navigation__link navigation__link_type_green-box" to="/signin">Войти</Link>
-          </ul>
-        }
-    </nav> */}
     </nav>
   );
 }

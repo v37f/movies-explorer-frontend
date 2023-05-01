bb789cd5bbf3ba82fb50adfbf0fcf5f09c26b5d9
Vladimir Frolov <frolov_vo@inbox.ru>
2023-05-01 16:35:14 +0300

    fix: change label attribute for to htmlFor

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -4,7 +4,7 @@ function FilterCheckbox() {
   return (
     <fieldset className="filter-checkbox">
       <input className="filter-checkbox__input" type="checkbox" name="short-films" id="short-films" value="short-films" />
-      <label className="filter-checkbox__label" for="short-films">Короткометражки</label>
+      <label className="filter-checkbox__label" htmlFor="short-films">Короткометражки</label>
     </fieldset>
   );
 }

2389696fb8c00eac12cc0a1cd3f0d666b6d60ad1
Vladimir Frolov <frolov_vo@inbox.ru>
2023-05-26 17:28:46 +0300

    feat: add trailer link to movie card

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -20,15 +20,17 @@ function MoviesCard({ movie }) {
   return (
     <li className="movie-card">
       {
-        pathname==="/movies" ?  
-          <button className={saveButtonClassName} type="button" aria-label="Сохранить" onClick={handleSaveClick} /> :
-          <button className="movie-card__button movie-card__button_type_delete" type="button" aria-label="Удалить" />
+          pathname==="/movies" ?  
+            <button className={saveButtonClassName} type="button" aria-label="Сохранить" onClick={handleSaveClick} /> :
+            <button className="movie-card__button movie-card__button_type_delete" type="button" aria-label="Удалить" />
       }
-      <img className="movie-card__image" src={`${MOVIES_BASE_URL}${movie.image.url}`} alt={movie.nameRU} />
-      <div className="movie-card__text-container">
-        <h2 className="movie-card__title">{movie.nameRU}</h2>
-        <p className="movie-card__duration">{formatTime(movie.duration)}</p>
-      </div>
+      <a className="movie-card__trailer-link" href={`${movie.trailerLink}`} target="_blank" rel="noreferrer" title="Смотреть трейлер">
+        <img className="movie-card__image" src={`${MOVIES_BASE_URL}${movie.image.url}`} alt={movie.nameRU} />
+        <div className="movie-card__text-container">
+          <h2 className="movie-card__title">{movie.nameRU}</h2>
+          <p className="movie-card__duration">{formatTime(movie.duration)}</p>
+        </div>
+      </a>
     </li>
   );
 }
